fc01696b4661b366f87a593993b20322f5c23067
Emmanuel Genard <edgenard@gmail.com>
2017-11-12 19:41:45 -0500

    Extract validate_amount method refactoring, step 2. Note: Check for nil must come first

--- validations_excercise/lib/expense.rb
+++ validations_excercise/lib/expense.rb
@@ -3,13 +3,17 @@ class Expense
         raise ValidationError.new('Name cannot be nil') if name == nil
         raise ValidationError.new('Name cannot be empty') if name == ''
         raise ValidationError.new('Name cannot be blank') if !name.match(/\S/)
-        raise ValidationError.new('Amount cannot be nil') if amount == nil
-        validate_amount if amount < 0
+        validate_amount(amount)
     end
 
-    def validate_amount(amount = :Delete_Me)
-        raise ValidationError.new('Amount cannot be less than zero')
+    def validate_amount(amount)
+        if amount == nil
+            raise ValidationError.new('Amount cannot be nil')
+        elsif amount < 0
+            raise ValidationError.new('Amount cannot be less than zero')
+        end
     end
+
    class ValidationError < StandardError    
    end 
 end
