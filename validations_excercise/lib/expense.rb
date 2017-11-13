fa80386f770a0b10beed01e85c068a1fe0db1c86
Emmanuel Genard <edgenard@gmail.com>
2017-11-12 19:53:32 -0500

    Extract validate_name method refacforing

--- validations_excercise/lib/expense.rb
+++ validations_excercise/lib/expense.rb
@@ -1,8 +1,6 @@
 class Expense
     def initialize(name:, amount:)
-        raise ValidationError.new('Name cannot be nil') if name == nil
-        raise ValidationError.new('Name cannot be empty') if name == ''
-        raise ValidationError.new('Name cannot be blank') if !name.match(/\S/)
+        validate_name(name)
         validate_amount(amount)
     end
 
@@ -11,6 +9,16 @@ class Expense
             raise ValidationError.new('Amount cannot be nil')
         elsif amount < 0
             raise ValidationError.new('Amount cannot be less than zero')
+        end
+    end
+
+    def validate_name(name)
+        if name == nil
+            raise ValidationError.new('Name cannot be nil')
+        elsif name == ''
+            raise ValidationError.new('Name cannot be empty')
+        elsif !name.match(/\S/)
+            raise ValidationError.new('Name cannot be blank')
         end
     end
 
