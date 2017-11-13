dc11c0008ec0d783486af17266b09aa0c1f12b2b
Emmanuel Genard <edgenard@gmail.com>
2017-11-12 19:37:07 -0500

    Extract validate_amount method refactoring, step 1

--- validations_excercise/lib/expense.rb
+++ validations_excercise/lib/expense.rb
@@ -4,7 +4,11 @@ class Expense
         raise ValidationError.new('Name cannot be empty') if name == ''
         raise ValidationError.new('Name cannot be blank') if !name.match(/\S/)
         raise ValidationError.new('Amount cannot be nil') if amount == nil
-        raise ValidationError.new('Amount cannot be less than zero') if amount < 0
+        validate_amount if amount < 0
+    end
+
+    def validate_amount(amount = :Delete_Me)
+        raise ValidationError.new('Amount cannot be less than zero')
     end
    class ValidationError < StandardError    
    end 
