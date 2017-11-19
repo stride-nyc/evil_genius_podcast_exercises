b5026787dfb04d46d4271b11dc5a424a69f8997e
Emmanuel Genard <edgenard@gmail.com>
2017-11-18 22:22:05 -0500

    Add check_for_blank and check_for_empty

--- validations_excercise/lib/expense.rb
+++ validations_excercise/lib/expense.rb
@@ -6,17 +6,12 @@ class Expense
 
     def validate_amount(validatable:, value:)
         message =  check_for_nil(value) || check_for_less_than_zero(value)
-        
+
         raise ValidationError.new("#{validatable} cannot be #{message}".capitalize) if message
     end
 
     def validate_name(validatable:, value:)
-        message = check_for_nil(value) || case value
-        when  ''
-            'empty'
-        when /^\s*$/
-            'blank'
-        end
+        message = check_for_nil(value) || check_for_empty(value) || check_for_blank(value)
 
         raise ValidationError.new("#{validatable} cannot be #{message}".capitalize) if message
     end
@@ -31,6 +26,18 @@ class Expense
         if value < 0
             'less than zero'
         end 
+    end
+
+    def check_for_empty(value)
+        if value == ''
+            'empty'
+        end
+    end
+
+    def check_for_blank(value)
+        if  /^\s*$/ === value
+            'blank'
+        end
     end
 
    class ValidationError < StandardError    
