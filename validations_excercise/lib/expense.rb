c38bf234918539fc7b2f06a4190aed90a39c7d76
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-12-18 08:48:29 -0500

    Move all name and amount validations
    - rename attribute param to value
    - add second param, attribute, in order to avoid conditional on line 4
    - distinguish between values by inspecting their class

--- validations_excercise/lib/expense.rb
+++ validations_excercise/lib/expense.rb
@@ -1,17 +1,18 @@
 class Expense
-  def validates(attribute)
-    raise ValidationError, 'Name cannot be nil' if attribute.nil?
-    raise ValidationError, 'Name cannot be empty' if attribute.empty?
-    raise ValidationError, 'Name cannot be blank' if attribute.strip.empty?
+  def validates(value, attribute)
+    if value.class == NilClass
+      raise ValidationError, "#{attribute.capitalize} cannot be nil" if value.nil?
+    elsif value.class == String
+      raise ValidationError, 'Name cannot be empty' if value.empty?
+      raise ValidationError, 'Name cannot be blank' if value.strip.empty?
+    elsif value.class == Integer
+      raise ValidationError, 'Amount cannot be less than zero' if value < 0
+    end
   end
 
   def initialize(name:, amount:)
-    validates(name)
-    if amount.nil?
-      raise ValidationError, 'Amount cannot be nil'
-    elsif amount < 0
-      raise ValidationError, 'Amount cannot be less than zero'
-    end
+    validates(name, :name)
+    validates(amount, :amount)
   end
 end
 
