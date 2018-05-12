5a1e53792e0ad95b14350e24a043d1d36696349b
Emmanuel Genard <edgenard@gmail.com>
2018-05-12 19:38:24 -0400

    Remove unnecessary comments. Stub out method that loads up video list

--- refactoring_external_service/youtube_video_list_spec.rb
+++ refactoring_external_service/youtube_video_list_spec.rb
@@ -1,7 +1,16 @@
 require 'minitest/autorun'
 require './youtube_video_list'
+require 'json'
 
-class VideoServiceTest < Test::Unit::TestCase
+class VideoServiceTest < MiniTest::Test
     
+    def test_video_list_returns_video_list
+        video_service = VideoService.new
+        video_array = [{'youtubeId': 'blahblahblah', 'views': 3, 'monthlyViews': 1}]
+        video_json = JSON.generate(video_array)
+        video_service.stub(:get_current_video_list, video_array) do
+            assert_equal(video_service.video_list, video_json)
+        end
+    end
     
 end
