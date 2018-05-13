c2fe51c5dffa0efa5d2cb84a66ec555473146692
Emmanuel Genard <edgenard@gmail.com>
2018-05-12 21:35:05 -0400

    Extract external service dependencies into method calls, get first real test

--- refactoring_external_service/youtube_video_list.rb
+++ refactoring_external_service/youtube_video_list.rb
@@ -2,23 +2,7 @@
   def video_list
     @video_list = get_current_video_list
     ids = @video_list.map{|v| v['youtubeID']}
-    # NOTE: External Service
-    client = GoogleAuthorizer.new(
-      token_key: 'api-youtube',
-      application_name: 'Gateway Youtube Example',
-      application_version: '0.1'
-      ).api_client
-    # NOTE: External Service
-    youtube = client.discovered_api('youtube', 'v3')
-    request = {
-      api_method: youtube.videos.list,# NOTE: External Service
-      parameters: {
-        id: ids.join(","),
-        part: 'snippet, contentDetails, statistics',
-      }
-    }
-    # NOTE: External Service 
-    response = JSON.parse(client.execute!(request).body)
+    response = get_youtube_stats_on_videos(ids)
     ids.each do |id|
       video = @video_list.find{|v| id == v['youtubeID']}
       youtube_record = response['items'].find{|v| id == v['id']}
@@ -30,8 +14,28 @@
   end
 
   private
+  
   def get_current_video_list
     video_list_json = File.read('videos.json')
     JSON.parse(video_list_json)
+  end
+
+  def get_youtube_stats_on_videos(youtube_ids)
+    client = GoogleAuthorizer.new(
+      token_key: 'api-youtube',
+      application_name: 'Gateway Youtube Example',
+      application_version: '0.1'
+      ).api_client
+    # NOTE: External Service
+    youtube = client.discovered_api('youtube', 'v3')
+    request = {
+      api_method: youtube.videos.list,# NOTE: External Service
+      parameters: {
+        id: youtube_ids.join(","),
+        part: 'snippet, contentDetails, statistics',
+      }
+    }
+    # NOTE: External Service 
+    response = JSON.parse(client.execute!(request).body)
   end
 end
--- refactoring_external_service/youtube_video_list_spec.rb
+++ refactoring_external_service/youtube_video_list_spec.rb
@@ -1,15 +1,31 @@
 require 'minitest/autorun'
 require './youtube_video_list'
 require 'json'
+require 'date'
 
 class VideoServiceTest < MiniTest::Test
     
     def test_video_list_returns_video_list
         video_service = VideoService.new
-        video_array = [{'youtubeId': 'blahblahblah', 'views': 3, 'monthlyViews': 1}]
+        video_array = [{'youtubeID' => 'blahblahblah', 'views' => 3, 'monthlyViews' => 3}]
+        youtube_response = {'items' => 
+               [
+                    {
+                        'id' =>'blahblahblah', 
+                        'statistics' => {'viewCount' => '3'}, 
+                        'snippet' => {'publishedAt' => (Date.today - 30).to_s }
+                    }
+                ]
+            }
         video_json = JSON.generate(video_array)
         video_service.stub(:get_current_video_list, video_array) do
-            assert_equal(video_service.video_list, video_json)
+            video_service.stub(:get_youtube_stats_on_videos, youtube_response) do
+                result = JSON.parse(video_service.video_list)
+                actual = JSON.parse(video_json)
+                assert_equal(result[0]['youtubeID'], result[0]['youtubeID'])
+                assert_equal(result[0]['views'], result[0]['views'])
+                assert_in_delta(result[0]['monthlyViews'], result[0]['monthlyViews'], 0.1)
+            end      
         end
     end
     
