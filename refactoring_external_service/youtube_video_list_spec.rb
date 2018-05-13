abbf9cbe64c9b934d4738b8fc09376a9f44c0fea
Emmanuel Genard <edgenard@gmail.com>
2018-05-13 12:41:36 -0400

    Make TestStubs able to return test specific canned responses

--- refactoring_external_service/youtube_video_list_spec.rb
+++ refactoring_external_service/youtube_video_list_spec.rb
@@ -6,29 +6,42 @@ require 'date'
 class VideoServiceTest < MiniTest::Test
 
     class VideoRepoStub
+        attr_reader :canned_response
+        def initialize(canned_response)
+            @canned_response = canned_response
+        end
         def videos
-            [{'youtubeID' => 'blahblahblah', 'views' => 3, 'monthlyViews' => 3}]
+           canned_response
         end
     end
 
     class YoutubeVideoClientStub
-        def video_stats(ids=:NotGiven)
-            {'items' => 
-                [
-                    {
-                        'id' =>'blahblahblah', 
-                        'statistics' => {'viewCount' => '3'}, 
-                        'snippet' => {'publishedAt' => (Date.today - 30).to_s }
-                    }
-                ]
-            }
+        attr_reader :canned_response
+        def initialize(canned_response)
+            @canned_response = canned_response
+        end
+
+        def video_stats(ids=:NotGivenToStub)
+            canned_response
         end
     end
     
     def test_video_list_returns_video_list
-        video_service = VideoService.new(video_repo: VideoRepoStub.new, video_client: YoutubeVideoClientStub.new)
-        video_array = [{'youtubeID' => 'blahblahblah', 'views' => 3, 'monthlyViews' => 3}]
-        video_json = JSON.generate(video_array)
+        video_repo_response = [{'youtubeID' => 'blahblahblah', 'views' => 3, 'monthlyViews' => 3}]
+        youtube_client_response =  {'items' => 
+            [
+                {
+                    'id' =>'blahblahblah', 
+                    'statistics' => {'viewCount' => '3'}, 
+                    'snippet' => {'publishedAt' => (Date.today - 30).to_s }
+                }
+            ]
+        }
+        video_service = VideoService.new(
+            video_repo: VideoRepoStub.new(video_repo_response), 
+            video_client: YoutubeVideoClientStub.new(youtube_client_response)
+        )
+        video_json = JSON.generate(video_repo_response)
 
         result = JSON.parse(video_service.video_list)
         actual = JSON.parse(video_json)
