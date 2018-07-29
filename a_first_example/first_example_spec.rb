7a45e9baff1e3e67b6ba28d87fc62600389a1011
Emmanuel Genard <edgenard@gmail.com>
2018-07-29 13:11:07 -0400

    Add test cases for #statement     - I had to assume that the statement worked as it is     - I asserted against whatever statement returned as of now     - I tried to cover all the cases and conditionals

---
+++ a_first_example/first_example_spec.rb
@@ -0,0 +1,114 @@
+require "minitest/autorun"
+require_relative 'first_example.rb'
+
+class TestFirstExample < Minitest::Test
+  def setup
+    @joe = Customer.new('Joe')
+    @child_movie = Movie.new('Cars', Movie::CHILDRENS)
+    @reg_movie = Movie.new('Jaws', Movie::REGULAR)
+    @new_movie = Movie.new('Tag', Movie::NEW_RELEASE)
+  end
+
+  def test_3day_child
+    @joe.add_rental(Rental.new(@child_movie, 3))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Cars\t1.5\nAmount owed is 1.5\n" +
+      "You earned 1 frequent renter points")
+  end
+
+  def test_5day_child
+    @joe.add_rental(Rental.new(@child_movie, 5))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Cars\t4.5\n" +
+      "Amount owed is 4.5\n" +
+      "You earned 1 frequent renter points")
+  end
+
+  def test_1day_new
+    @joe.add_rental(Rental.new(@new_movie, 1))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Tag\t3\n" +
+      "Amount owed is 3\n" +
+      "You earned 1 frequent renter points")
+  end
+
+  def test_2day_new
+    @joe.add_rental(Rental.new(@new_movie, 2))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Tag\t6\n" +
+      "Amount owed is 6\n" +
+      "You earned 2 frequent renter points")
+  end
+
+  def test_3day_reg
+    @joe.add_rental(Rental.new(@reg_movie, 3))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Jaws\t3.5\n" +
+      "Amount owed is 3.5\n" +
+      "You earned 1 frequent renter points")
+  end
+
+  def test_3day_reg_3day_new
+    @joe.add_rental(Rental.new(@reg_movie, 3))
+    @joe.add_rental(Rental.new(@new_movie, 3))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Jaws\t3.5\n\t" +
+      "Tag\t9\n" +
+      "Amount owed is 12.5\n" +
+      "You earned 3 frequent renter points")
+  end
+
+  def test_3day_reg_4day_child
+    @joe.add_rental(Rental.new(@reg_movie, 3))
+    @joe.add_rental(Rental.new(@child_movie, 4))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Jaws\t3.5\n\t" +
+      "Cars\t3.0\n" +
+      "Amount owed is 6.5\n" +
+      "You earned 2 frequent renter points")
+  end
+
+  def test_2day_new_4day_child
+    @joe.add_rental(Rental.new(@new_movie, 2))
+    @joe.add_rental(Rental.new(@child_movie, 4))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Tag\t6\n\t" +
+      "Cars\t3.0\n" +
+      "Amount owed is 9.0\n" +
+      "You earned 3 frequent renter points")
+  end
+
+  def test_2day_new_4day_child_3day_reg
+    @joe.add_rental(Rental.new(@new_movie, 2))
+    @joe.add_rental(Rental.new(@child_movie, 4))
+    @joe.add_rental(Rental.new(@reg_movie, 3))
+
+    assert_equal(@joe.statement,
+      "Rental Record for Joe\n\t" +
+      "Tag\t6\n\t" +
+      "Cars\t3.0\n\t" +
+      "Jaws\t3.5\n" +
+      "Amount owed is 12.5\n" +
+      "You earned 4 frequent renter points")
+  end
+
+end
+
+
+
