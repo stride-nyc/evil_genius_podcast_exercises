2bb71cf34ad8eebcf80897523b159b3623c7aad6
Emmanuel Genard <edgenard@gmail.com>
2018-07-29 17:34:39 -0400

    rename element to rental

--- a_first_example/first_example.rb
+++ a_first_example/first_example.rb
@@ -36,28 +36,28 @@ class Customer
   def statement
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
-    @rentals.each do |element|
+    @rentals.each do |rental|
       this_amount = 0
 
       # determine amounts for each line
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+      case rental.movie.price_code
+        when Movie::REGULAR
+          this_amount += 2
+          this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+        when Movie::NEW_RELEASE
+          this_amount += rental.days_rented * 3
+        when Movie::CHILDRENS
+          this_amount += 1.5
+          this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
       end
 
       # add frequent renter points
       frequent_renter_points += 1
       # add bonus for a two day new release rental
-      frequent_renter_points += 1 if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      frequent_renter_points += 1 if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
 
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
+      result += "\t" + rental.movie.title + "\t" + this_amount.to_s + "\n"
       total_amount += this_amount
     end
     # add footer lines
